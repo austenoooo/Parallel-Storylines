e45c43c936dd0758d7e0ee5df29afd3bc155fe0b
Austenoooo <yixuan.li683@dukekunshan.edu.cn>
2023-09-17 16:24:37 -0400

    created files

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// the start of the story
+let story = "Beneath the twinkling stars, a lone wolf howled in the heart of the wilderness.";
+
+const storyDiv = document.getElementById("story");
+storyDiv.innerHTML = story + "...";
+
+const generateButton = document.getElementById("generate");
+const keywordsInput = document.getElementById("keywords");
+
+
